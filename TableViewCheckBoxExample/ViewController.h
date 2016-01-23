008355a278b75c3bcc9b4ffbf38d85b026fdd585
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2016-01-23 19:10:05 +0530

    This is a simple example of UITableView default check box.

--- TableViewCheckBoxExample/ViewController.h
+++ TableViewCheckBoxExample/ViewController.h
@@ -8,8 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
+{
+    NSMutableArray *dataArray;
+}
 
+@property (weak, nonatomic) IBOutlet UITableView *mTableView;
+- (IBAction)didTapBringCheckBoxBtn:(id)sender;
 
 @end
 
--- TableViewCheckBoxExample/ViewController.m
+++ TableViewCheckBoxExample/ViewController.m
@@ -16,12 +16,72 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    dataArray=[[NSMutableArray alloc]init];
+    [dataArray addObject:@"Apple"];
+    [dataArray addObject:@"Mango"];
+    [dataArray addObject:@"Papaya"];
+    [dataArray addObject:@"Guava"];
+    [dataArray addObject:@"Pineapple"];
+    [dataArray addObject:@"Strawberry"];
+    [dataArray addObject:@"Banana"];
+    [dataArray addObject:@"Grapes"];
+    [dataArray addObject:@"Pomegranate"];
+    [dataArray addObject:@"Green Tea"];
+    [dataArray addObject:@"Raisin"];
+    
+    self.mTableView.delegate=(id)self;
+    self.mTableView.dataSource=(id)self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Check Box Button Action
+
+- (IBAction)didTapBringCheckBoxBtn:(id)sender {
+    
+    [self.mTableView setEditing:YES animated:YES];
+    
+}
+
+#pragma mark - UITableView DataSource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [dataArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
+#pragma mark - UITableView Delegate Methods
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 3;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"user selected %@",[dataArray objectAtIndex:indexPath.row]);
+}
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+     NSLog(@"user de-selected %@",[dataArray objectAtIndex:indexPath.row]);
 }
 
 @end
